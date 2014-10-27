450dcea618759745080fa842bc39c5f325835838
Justin <zertukis@gmail.com>
2014-10-27 01:06:37 -0500

    merged both controllers together, made api call via angular $http and also the images are displayed/removed via ng-repeat now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,68 +8,48 @@
  * Controller of the imgurrandomApp
  */
 
+//global variable default init
 var subreddit = 'cats';
 var imageArray;
-var currentPage = 1;
+var currentPage = 0;
 var testing;
 var sortOrder = 'top';
 
-//ajax call for imgur api to get the gallery needed, temporary
-function ajaxCall(page) {
-	$.ajax({
-	authorization: '8c4fdcec50df594',
-	type: 'GET',
-	dataType: 'json',
-	headers: {
-		Authorization: 'Client-ID ' + '8c4fdcec50df594',
-		Accept: 'application/json'
-	},
-	url: 'https://api.imgur.com/3/gallery/r/' + subreddit + '/' + sortOrder + '/page/' + page + '.json',
-	data: {
-
-	},
-	success: function(json){
-		imageArray = [];
-		for (var i = 0; i < 60; i++) {		
-			imageArray[i] = 'http://i.imgur.com/' + json.data[i].id + 'm.png';
-			// testing = 'http://i.imgur.com/' + imageArray[i] + 'm.png';
-			// imageArray[i] = imageArray[i].replace(/"/g, "");
-			$('#imageContainer').append('<img style = "margin: 5px; height: auto; width: 200px" id = "galleryImages" src =' + imageArray[i] + '>');
-		}
-	},
-	error: function() {
-				alert('error');
-			}
-	});
-}
-
-
 angular.module('imgurRandomApp')
 
-//controls all input relating to searching: subreddit, new/top, order
-	.controller('SearchCtrl',['$scope', function ($scope) {
+//controls all input relating to searching: subreddit, new/top, order, page switch, and the api call
+	.controller('SearchCtrl',['$scope', '$http', function ($scope, $http) {		
+
+		$scope.apiCall = function() {
+			$http.defaults.headers.common['Authorization'] = 'Client-ID 8c4fdcec50df594';
+			$http.get('https://api.imgur.com/3/gallery/r/' + subreddit + '/' + sortOrder + '/page/' + currentPage + '.json', {headers: {'Authorization': 'Client-ID ' + '8c4fdcec50df594'}}).
+			success (function(json) {
+				imageArray = [];
+				for (var i = 0; i < 60; i++) {		
+					imageArray[i] = 'http://i.imgur.com/' + json.data[i].id + 'm.png';
+					$scope.imageArray = imageArray;
+
+				}
+			}).
+			error (function() {
+				alert('error');
+
+			})
+		};
+
 		$scope.enterSubreddit = function () {
 			subreddit = $scope.subreddit;
 			currentPage = 0;
-			ajaxCall(currentPage);
-			$scope.imageArray = ['test', '1', '3'];
-			console.log($scope.imageArray);
+			$scope.apiCall();			
 		};
 
 		$scope.subredditSort = function() {
 			sortOrder = $scope.sortBy;
-		}
-	}]);
+		};
 
-
-angular.module('imgurRandomApp')
-
-//controls moving forward/back through pages
-	.controller('PageCtrl', ['$scope', function ($scope) {
 		$scope.loadMore = function () {
 			currentPage = currentPage + 1;
-			ajaxCall(currentPage);
-
+			$scope.apiCall();
 		};
 		$scope.loadPrevious = function() {
 			if (currentPage === 0) {
@@ -77,7 +57,7 @@ angular.module('imgurRandomApp')
 			}
 			else {
 				currentPage = currentPage - 1;
-				ajaxCall(currentPage);
-			}
+				$scope.apiCall();			}
 		};
+
 	}]);
