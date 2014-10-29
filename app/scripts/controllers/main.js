46ed212c3932caaafb46a99caff93d1927ccb418
Justin <zertukis@gmail.com>
2014-10-29 01:17:41 -0500

    now displays title, views, date(needs to be fixed) with the image, did most of the css so it looks pretty now

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,6 +15,9 @@ var currentPage = 0;
 var pagePlusOne;
 var testing;
 var sortOrder = 'top';
+var titleArray =[];
+var infoArray = [];
+var linkArray = [];
 
 angular.module('imgurRandomApp')
 
@@ -28,14 +31,22 @@ angular.module('imgurRandomApp')
 			$http.get('https://api.imgur.com/3/gallery/r/' + subreddit + '/' + sortOrder + '/page/' + currentPage + '.json', {headers: {'Authorization': 'Client-ID ' + '8c4fdcec50df594'}}).
 			success (function(json) {
 				imageArray = [];
-				for (var i = 0; i < 60; i++) {		
+				for (var i = 0; i < 60; i++) {
+					infoArray[i] = json.data[i];		
 					imageArray[i] = 'http://i.imgur.com/' + json.data[i].id + 'm.png';
+					infoArray[i].id = imageArray[i];
+					$scope.infoArray = infoArray;
 					$scope.imageArray = imageArray;
+					titleArray[i] = json.data[i].title;
+					$scope.titleArray = titleArray;
+					linkArray[i] = json.data[i].link;
+					$scope.linkArray = linkArray;
+
 
 				}
 			}).
 			error (function() {
-				alert('error');
+				alert('Select a sort by option');
 
 			})
 		};
@@ -56,7 +67,7 @@ angular.module('imgurRandomApp')
 		};
 		$scope.loadPrevious = function() {
 			if (currentPage === 0) {
-				alert('Already on First Page! Click Next!');
+				alert('Already on First Page! Click Next');
 			}
 			else {
 				currentPage = currentPage - 1;
